cbbc1be81e0d77ac585e04a218c45b2e6804114b
Efren <efrenbarragan606@gmail.com>
2023-09-03 15:58:04 -0500

    Nuevo componenete

---
+++ js/main.js
@@ -0,0 +1,16 @@
+    newFunction();
+
+function newFunction() {
+    const miElemento = document.getElementById("miElemento");
+    const miBoton = document.getElementById("miBoton");
+
+    miBoton.addEventListener("click", function () {
+        if (miElemento.style.display === "none" || miElemento.style.display === "") {
+            // Si el elemento está oculto, mostrarlo
+            miElemento.style.display = "block";
+        } else {
+            // Si el elemento está visible, ocultarlo
+            miElemento.style.display = "none";
+        }
+    });
+}
